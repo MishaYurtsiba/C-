2c1e03ffda14403c1c009b5be50346f25b03d7bd
MishaYurtsiba <73959369+MishaYurtsiba@users.noreply.github.com>
2021-03-14 18:22:02 +0200

    Add files via upload

---
+++ WForms_5/WindowsForms_5 1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_5_1
+{
+    public partial class Form1 : Form
+    {
+        static int Nsd(int a, int b)
+        {
+            while (b != 0)
+                b = a % (a = b);
+            return a;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int a = Convert.ToInt32(textBox1.Text);
+            
+            int b = Convert.ToInt32(textBox2.Text);
+            int s = Nsd(a, b) + Nsd(a, 4) + Nsd(24, b);
+            textBox3.Text = s.ToString();
+        }
+    }
+}
---
+++ WForms_5/WindowsForms_5 3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_5_3
+{
+    public partial class Form1 : Form
+    {
+        static double Rec(double n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1 || n == 2 || n == 3)
+            {
+                return 7;
+            }
+            else
+            {
+                return Rec(n - 1) * (1 + Rec(n - 2) + Rec(n - 3) / Rec(n - 4));
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            double n = Convert.ToDouble(textBox1.Text);
+            double s = Rec(n);
+            textBox2.Text = s.ToString();
+        }
+    }
+}
