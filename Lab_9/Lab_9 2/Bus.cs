9f3e8228a0c16e0936508c38171564d3f6d06b8c
MishaYurtsiba <73959369+MishaYurtsiba@users.noreply.github.com>
2021-04-04 19:08:17 +0300

    Add files via upload

---
+++ Lab_9/Lab_9 2/Bus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9_2
+{
+    class Bus
+    {
+        public string mark;
+        public int mas;
+        public int SeatsNamber;
+        public int PassengerNaber;
+
+        public Bus(string mark, int mas, int SeatsNamber,int PassengerNaber)
+        {
+            this.mark =mark;
+            this.mas = mas;
+            this.SeatsNamber = SeatsNamber;
+            this.PassengerNaber = PassengerNaber;
+        }
+        public int Mmas()
+        {
+            return mas + (PassengerNaber * 62);
+        }
+
+    }
+}
---
+++ Lab_9/Lab_9_2/Bus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9_2
+{
+    class Bus
+    {
+        public string mark { get; set; }
+        public int mas { get; set; }
+        public int SeatsNamber { get; set; }
+        public int PassengerNaber { get; set; }
+
+        public Bus(string mark, int mas, int SeatsNamber, int PassengerNaber)
+        {
+            this.mark = mark;
+            this.mas = mas;
+            this.SeatsNamber = SeatsNamber;
+            this.PassengerNaber = PassengerNaber;
+        }
+        public int Mmas()
+        {
+            return mas + (PassengerNaber * 62);
+        }
+    }
+}
