33b60d44429d6c0fa9aa9455c4ae55f4e31f2cd3
MishaYurtsiba <73959369+MishaYurtsiba@users.noreply.github.com>
2021-04-04 19:09:50 +0300

    Add files via upload

---
+++ Lab_11/Lab_11 1/SEquation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11_1
+{
+    class SEquation: TEquation_
+    {
+        public double Arr()
+        {
+
+
+            int n = 5;
+            double sum = 0;
+            Random rand = new Random(DateTime.Now.Millisecond);
+            double[] A = new double[n];
+            double[] B = new double[n];
+            double[] C = new double[n];
+            for(int i = 0; i < n; i++)
+            {
+                A[i] = rand.Next(-100, 100);
+                B[i] = rand.Next(-100, 100);
+                C[i] = rand.Next(-100, 100);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                double d = Math.Pow(B[i], 2) - 4 * A[i] * C[i];
+                if (d > 0)
+                {
+                    double x1 = (-B[i] - d) / 2 * A[i];
+                    double x2 = (-B[i] + d) / 2 * A[i];
+                    sum += x1 + x2;
+                }
+                else if (d == 0)
+                {
+                    double x = -B[i] / 2 * A[i];
+                    sum += x;
+                }
+            }
+            return sum;
+
+        }
+    }
+}
---
+++ Lab_11/Lab_11 1/TEquation,.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11_1
+{
+    class TEquation_
+    {
+        double b;
+        double c;
+        double n;
+        public TEquation_()
+        {
+
+        }
+        public TEquation_(double b,double c, double n)
+        {
+            this.b = b;
+            this.c = c;
+            this.n = n;
+        }
+        public string Solution()
+        {
+            string v = "";
+            if (b == 0 && c == 0)
+            {
+                v = "Рівняння має безліч коркнів";
+            }
+            else if (b == 0)
+            {
+                v = "Рівняння немає коренів";
+            }
+            else if (b != 0)
+            {
+                double f = (c / b);
+                v = $"{f}";
+            }
+            return v;
+        }
+        public string Comparison()
+        {
+            string v = "";
+            double f = (c / b);
+            if (n == f)
+            {
+                v = "Беде коренем рівняння";
+            }
+            else
+            {
+                v = "Не буде коренем рівнянням";
+            }
+            return v;
+        }
+        public double Equ()
+        {
+            int n = 5;
+            double sum = 0;
+            Random rand = new Random(DateTime.Now.Millisecond);
+            double[] A = new double[n];
+            double[] B = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                A[i] = rand.Next(-100, 100);
+                B[i] = rand.Next(-100, 100);               
+            }
+            for (int i = 0; i < n; i++)
+            {
+                double f = A[i] / B[i];
+                sum += f;
+            }
+            return sum;
+        }
+    }
+}
